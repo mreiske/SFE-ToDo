39378e4c7eaf96ef6ff1ae5609eb3ebfd4c0d697
mreiske <monique.reiske@hu-berlin.de>
2016-08-24 12:18:00 +0200

    adjust to styleguide

--- QID-597-SFEacfar1/SFEacfar1.R
+++ QID-597-SFEacfar1/SFEacfar1.R
@@ -10,14 +10,14 @@ a   = "0.9"  # value of alpha_1
 # Input alpha_1
 message = "      give alpha"
 default = a
-a = winDialogString(message, default)
-a = type.convert(a, na.strings = "NA", as.is = FALSE, dec = ".")
+a       = winDialogString(message, default)
+a       = type.convert(a, na.strings = "NA", as.is = FALSE, dec = ".")
 
 # Input lag value
 message = "      give lag"
 default = lag
-lag = winDialogString(message, default)
-lag = type.convert(lag, na.strings = "NA", as.is = FALSE, dec = ".")
+lag     = winDialogString(message, default)
+lag     = type.convert(lag, na.strings = "NA", as.is = FALSE, dec = ".")
 
 # Plot
 plot(ARMAacf(ar = a, ma = numeric(0), lag.max = lag, pacf = FALSE), type = "h", 
